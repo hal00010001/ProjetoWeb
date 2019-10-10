d083a545d8e871698f2a6d371352cf3feb48aaea
Almir Loiola Bindo <hal0001@gmail.com>
2019-10-10 00:53:32 -0300

    Commit

---
+++ PDO/CrudPDO.php
@@ -0,0 +1,45 @@
+<?php
+
+class CrudPDO extends Conexao {
+    
+    //Atributos
+    private $crud;
+    private $contador;
+    
+    //Preparação das declarações
+    private function preparedStatements($query, $parametros){
+        $this->countParametros($parametros);
+        $this->crud = $this->conectaDB()->prepare($query);
+        if($this->contador > 0){
+            for($i=1; $i<=$this->contador; $i++){
+                $this->crud->bindValue($i, $parametros[$i - 1]);
+            }
+        }
+        $this->crud->execute();
+        //echo $this->contador;
+    }
+    
+    //Contador de parâmetros
+    private function countParametros($parametros){
+        $this->contador = count($parametros);
+    }
+    
+    //Insercao no banco de dados
+    public function insertDB($tabela, $condicao, $parametros){        
+        $this->preparedStatements("insert into {$tabela} values ({$condicao})", $parametros);
+        return $this->crud;
+    }
+    
+    //Insercao no banco de dados
+    public function selectDB($campos, $tabela, $condicao, $parametros){        
+        $this->preparedStatements("select {$campos} from {$tabela} {$condicao}", $parametros);
+        return $this->crud;
+    }
+    
+    //Deletar dados no DB
+    public function deleteDB($tabela, $condicao, $parametros){
+        $this->preparedStatements("delete from {$tabela} where {$condicao}", $parametros);
+        return $this->crud;
+    }
+    
+}
