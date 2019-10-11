2a1062b1c9e5f113b1705598e906b7087109fd85
Almir Loiola Bindo <hal0001@gmail.com>
2019-10-11 16:49:33 -0300

    Commit

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Escola de Idiomas</title>
+        <link rel="stylesheet" href="resources/css/style.css">        
+    </head>
+    <body>
+        <div class="banner">
+            <!--<img src="images/banner.jpg" alt="">-->
+        </div>
+        <div class="nav">
+            <li><a href="pages/professor.php">Professor</a></li>
+            <li><a href="pages/aluno.php">Aluno</a></li>
+            <li><a href="pages/materia.php">Materia</a></li>
+            <li><a href="pages/turma.php">Turma</a></li>
+            <li><a href="pages/notas.php">Notas</a></li>
+            <li><a href="pages/presenca.php">Presen&ccedil;a</a></li>
+        </div>
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            define('ROOT_PATH', dirname(__FILE__));
-            require_once ROOT_PATH.'/Controller/ControllerProfessor.php';            
-        ?>
-        <a href="Controller/ControllerProfessor.php">Cadastrar Professor</a>
-    </body>
-</html>
+<?php include("includes/header.php"); ?>
+<div class="content">
+    <?php
+        define('ROOT_PATH', dirname(__FILE__));
+    ?>
+</div>
+<?php include("includes/footer.php"); ?>
