c59dac57e673540c9d3a067841f3c9e56a6c6afd
Almir Loiola Bindo <hal0001@gmail.com>
2019-10-28 00:01:19 -0300

    Commit

--- PDO/Conexao.php
+++ PDO/Conexao.php
@@ -5,7 +5,7 @@ abstract class Conexao {
     //Realizara a conexao com o banco de dados
     protected function conectaDB(){
         try {
-            $Con = new PDO("mysql:host=localhost;dbname=idioma", "root", "");
+            //$Con = new PDO("mysql:host=localhost;dbname=idioma", "root", "");
             $Con = new PDO("mysql:host=localhost;dbname=id11380331_idioma", "root_idioma", "Root_IdiomaZ");            
             return $Con;
         } catch (PDOException $ex) {
